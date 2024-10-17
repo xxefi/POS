63fb5c432e801767c3e1994ef24ba3816aaa70b8
xxefi <magsudluefgan@gmail.com>
2024-10-17 16:22:27 +0400

    1

--- Frontend(Cashier)/cashier_react/src/layouts/layout.tsx
+++ Frontend(Cashier)/cashier_react/src/layouts/layout.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <div className="flex">
-      <a href="/login">login</a>
+      <Link to="/login">Login</Link>
       <div className="flex-grow p-4 flex flex-col mt-4">
         <Outlet />
       </div>
